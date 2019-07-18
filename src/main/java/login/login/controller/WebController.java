d311b8b610b6044381495edbe50195a965c31e1d
DEVHARAM <gkfka5968@naver.com>
2019-07-18 19:52:38 +0900

    Initial commit

---
+++ src/main/java/login/login/controller/WebController.java
@@ -0,0 +1,16 @@
+package login.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping(value = "/webController.do")
+    public String helloWorld(ModelMap modelMap) {
+        modelMap.addAttribute("author", "CodingSquid");
+        return "index";
+    }
+
+}
---
+++ web/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: gkfka
+  Date: 2019-07-06
+  Time: 오후 7:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>${author}</h1>
+Hello World! Spring 4 By CodingSquid
+</body>
+</html>
